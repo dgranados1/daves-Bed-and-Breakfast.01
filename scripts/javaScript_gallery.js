a33efd9f1dd6de373cea642e57e88b50c3f6fb07
dgranados1 <dgluna@amarillocollege.com>
2026-04-19 19:17:51 -0500

    Add files via upload

---
+++ scripts/javaScript_gallery.js
@@ -0,0 +1,41 @@
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+let lightboxTitle = "My Gallery";
+
+// Names of the image files shown in the slideshow
+let imgFiles = [
+  "images/photo_1.png",
+  "images/photo_2.png",
+  "images/photo_3.png",
+  "images/photo_4.png",
+  "images/photo_5.png",
+  "images/photo_6.png",
+  "images/photo_7.png"
+];
+
+// Captions associated with each image
+let imgCaptions = [
+  "Oceanfront breakfast",
+  "Couple enjoying gourmet meal",
+  "Live island music night",
+  "Family enjoying express room",
+  "Surf coach teaching family how to surf",
+  "Our wonderful staff",
+  "David the owner creating our bed-and-breakfast"
+];
+
+
+// Count of images in the slideshow
+let imgCount = imgFiles.length;
